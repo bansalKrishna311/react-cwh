c3b7c4b8f833c531142c67f0c589669d5c787818
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-01-24 15:53:05 +0530

    dark mode setup hora hei

--- tut5/src/App.js
+++ tut5/src/App.js
@@ -3,21 +3,27 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/about';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-   <>
-   <Navbar title = "TextUtils" aboutText = "About"/>
-   {/* <Navbar /> */}
-   <div className="container">
-   <TextForm heading = "Enter the text to analyze Below" />
-   
-    </div>
-    
-    <About/ >
-    </>
+  const [mode, setMode] = useState('light');
+  const ToggleMode = () => {
+   if(mode === 'light')
+   {
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+   }
+  };
 
- );
+  return (
+    <>
+      <Navbar title="TextUtils" mode={Mode} ToggleMode={ToggleMode} />
+      <div className="container">
+        <TextForm heading="Enter the text to analyze Below" />
+      </div>
+      <About />
+    </>
+  );
 }
 
 export default App;
