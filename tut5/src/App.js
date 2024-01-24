9ef95dbff49bb76076bd6217e9d808a79911db9d
bansalKrishna311 <bansalkrishna311@gmail.com>
2024-01-24 16:53:02 +0530

    alerts strated wornking for the dark mode

--- tut5/src/App.js
+++ tut5/src/App.js
@@ -2,28 +2,41 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/about';
+import {useState} from 'react';
+
+
+// import React, { useState } from 'react'
 import Alert from './components/Alert';
 
-import React, { useState } from 'react';
 
 function App() {
   const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message,type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+  }
   const ToggleMode = () => {
    if(mode === 'light')
    {
       setMode('dark');
       document.body.style.backgroundColor ='#313840';
+      showAlert('Dark Mode has been enabled',"success");
    }
    else {
       setMode('light');
       document.body.style.backgroundColor ='white';
+      showAlert('Light Mode has been enabled',"success");
+
    }
   };
 
   return (
     <>
       <Navbar title="TextUtils" mode={mode} ToggleMode={ToggleMode} />
-      <Alert Alert = "Ram ram "/>
+     <Alert alert = {alert}/>
       <div className="container">
         <TextForm heading="Enter the text to analyze Below" mode={mode} />
       </div>
